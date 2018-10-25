8689724e4c8b268d59ccad932a17a47fd0fff4d5
romainsrcr <romain.sercer@outlook.fr>
2018-10-25 10:47:25 +0200

    Beginning of project

---
+++ myHouse/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  myHouse
+//
+//  Created by Romain SERCER on 25/10/2018.
+//  Copyright © 2018 fr.myHouse. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ myHouse/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  myHouse
+//
+//  Created by Romain SERCER on 25/10/2018.
+//  Copyright © 2018 fr.myHouse. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
