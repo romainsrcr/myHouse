44a801629288090e4e0e21de2d0ff2622990d405
romainsrcr <romain.sercer@outlook.fr>
2018-11-01 19:59:06 +0100

    Add map branch to visualize the IoT devices on a map, centered on Copenhagen region area

---
+++ myHouse/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  myHouse
+//
+//  Created by Romain SERCER on 25/10/2018.
+//  Copyright © 2018 fr.myHouse. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    // set initial location in Copenhagen
+    let initialLocation = CLLocation(latitude: 55.676098, longitude: 12.568337)
+    // set inital scale of 10 000 m
+    let regionRadius: CLLocationDistance = 10000
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        centerMapOnLocation(location: initialLocation)
+    }
+}
+
+
