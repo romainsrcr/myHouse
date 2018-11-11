bc69db94aa876948d76e69789422d94935f20c40
loic.chacornac <loic.chacornac@loicchacornacs-Mac.local>
2018-11-11 13:40:26 -0800

    Adaptation for all format

--- myHouse/DeviceCollectionViewController.swift
+++ myHouse/DeviceCollectionViewController.swift
@@ -15,8 +15,24 @@ class DeviceCollectionViewController: UIViewController, UICollectionViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.delegate = self;
-        collectionView.dataSource = self;
+        
+        let cellWidth: CGFloat
+        switch UIDevice.current.userInterfaceIdiom {
+        case .phone:
+            cellWidth = ((view.frame.size.width - 20) / 2) - 20
+        case .pad:
+            cellWidth = ((view.frame.size.width - 20) / 3) - 20
+        case .unspecified:
+            cellWidth = ((view.frame.size.width - 20) / 2) - 20
+        case .tv:
+            cellWidth = ((view.frame.size.width - 20) / 5) - 20
+        case .carPlay:
+            cellWidth = ((view.frame.size.width - 20) / 4) - 20
+        }
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: cellWidth, height: cellWidth)
+        
         Application.getDevices(success: {(devices) -> Void in self.devices = devices; self.collectionView.reloadData()} )
         
     }
