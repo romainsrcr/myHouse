3a8de625f727aeac07bc149640c6c389c0af0d38
betameche111 <lolo.betameche@hotmail.fr>
2018-12-06 12:04:38 +0100

    Add the decoder generator

--- myHouse/Tools/AppDelegate.swift
+++ myHouse/Tools/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         Switcher.updateRootVC()
         Application.getDevices(success: {})
+        Application.getAdvancedMode(success: {})
         return true
     }
 
